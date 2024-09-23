ef63d20940394c3d4669be2704a1216e58c67cb6
oscarsanvid <oscarsanvid04@gmail.com>
2024-09-23 21:17:24 +0200

    Programa Base: Calculadora Básica

---
+++ Calculator.Java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        // Código inicial que será ampliado por los desarrolladores
+        System.out.println("Bienvenido a la Calculadora Básica.");
+        scanner.close();
+    }
+}
